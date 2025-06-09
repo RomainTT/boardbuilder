cca0416f25ee0c82b16375a0920010cd83955484
Richard Stuart <richard.k.stuart@gmail.com>
2025-06-09 21:34:02 +0100

    staging and environmental updates

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+
+  globalSymbolsBase: 'https://app.gs-stage.co.uk',
+
+  globalSymbolsApiBase: 'https://app.gs-stage.co.uk/api/v1',
+  boardBuilderApiBase: 'https://app.gs-stage.co.uk/api/boardbuilder/v1',
+
+  globalSymbolsOauthClientId: 'RKBryis4wFurzoH14-J9gMiHbXHRll6YHTZXrYVJc_A'
+};
